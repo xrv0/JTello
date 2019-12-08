ba9904b8497e55b1e68bf4b01de966b57777a947
venum <mgautam@protonmail.com>
2019-12-08 13:45:54 +0100

    Fixed bug when receiving data

--- src/test/Main.java
+++ src/test/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String args[]) {
         TelloSendResponseConnection connection = new TelloSendResponseConnection();
         connection.enterSDKMode();
-        //System.out.println(connection.takeoff());
         System.out.println(connection.sendAndReceiveCommand("temp?"));
         connection.close();
     }
